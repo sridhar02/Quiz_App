6f7a53c17cd2f63b29b33466f9aa70431f3f2d3a
sridhar02 <kattasridhar02@gmail.com>
2020-07-21 18:27:19 +0530

    Updates

---
+++ precache-manifest.61a9c44e86195ac1140050c7965bb6e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91678d1e114d4db93fc94e23f569919a",
+    "url": "/Quiz_App/index.html"
+  },
+  {
+    "revision": "017d808d30c0fa002cee",
+    "url": "/Quiz_App/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "017d808d30c0fa002cee",
+    "url": "/Quiz_App/static/js/2.dbb3ce09.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Quiz_App/static/js/2.dbb3ce09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a6bf6e6ebc40e0b5fb3",
+    "url": "/Quiz_App/static/js/main.878d2179.chunk.js"
+  },
+  {
+    "revision": "b355d802d6e98137f320",
+    "url": "/Quiz_App/static/js/runtime-main.7245c2e7.js"
+  }
+]);
